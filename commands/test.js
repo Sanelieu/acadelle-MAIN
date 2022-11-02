e2ed2b330b56028e7cccc73b01183b0db8b80967
Sanelieu <sanelieuu@gmail.com>
2022-11-02 19:40:19 -0400

    beta

--- commands/test.js
+++ commands/test.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'test',
     description: 'Test ping command',
     execute(message, args){
-        message.channel.send ('Test complete, how did I do? ^^');
+        message.channel.send ('Hello! ^^, do you need something?');
     }
 }
