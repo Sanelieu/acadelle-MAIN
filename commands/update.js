1aa3bcbaeb9877e20d3ccc5db01cf2e75630ceff
sanelieu <sanelieuu@gmail.com>
2022-11-03 00:23:23 -0400

    v.013

--- commands/update.js
+++ commands/update.js
@@ -10,7 +10,7 @@ module.exports = {
         .setURL('')
         .setDescription('Bot version: v0.1 (beta)')
         .addFields(
-            {name:'💜 New commands available: 💜', value: '.test \n.support \n.update'}
+            {name:'💜 New commands available: 💜', value: '.test \n.support \n.update \n.permittest'}
         )
         .setImage('https://i.pinimg.com/originals/1e/68/4d/1e684d15ad21997f1a92adfae922cfe5.gif');
         
