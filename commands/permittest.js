d5a97fea7e338de88d1ed49b84c7413e19b59d01
Sanelieu <sanelieuu@gmail.com>
2022-11-03 01:07:53 -0400

    v.014

--- commands/permittest.js
+++ commands/permittest.js
@@ -1,13 +1,19 @@
 // tier 4 - permits
 
 const Discord = require("discord.js");
-const permitID = ("896962315512086549", "937418844966879243", "877940496352301096")
+const permitID = [
+    "896962315512086549", // ln.4
+    "937418844966879243", // ln.5
+    "877940496352301096" // ln.6
+];
+
 
 module.exports = {
     name: 'permittest',
     description: 'blank command',
     execute(message, args){
-        if (message.author.id !== permitID) return;
+        if (!permitID.includes(message.author.id)) return;
+
         message.channel.send ('seems that you have a permit ^^');
     }
 }
